d9a8ace9540f183386e6d9c914063c15f56cd668
howish <howardshih617038@gmail.com>
2020-09-20 21:34:43 +0800

    Rearrange code in list.c

--- quiz/week1/list.c
+++ quiz/week1/list.c
@@ -12,15 +12,13 @@ void add_entry(node_t **head, int new_value)
     node_t **indirect = head;
 
     node_t *new_node = malloc(sizeof(node_t));
+    assert(new_node); //AA1
     new_node->value = new_value;
     new_node->next = NULL;
 
-    //AA1 = (a)
-    assert(new_node);
     while (*indirect)
         indirect = &(*indirect)->next;
-    //AA2 = (b)
-    *indirect = new_node;
+    *indirect = new_node; //AA2
 }
 
 node_t *find_entry(node_t *head, int value)
@@ -45,12 +43,10 @@ void remove_entry(node_t **head, node_t *entry)
 node_t *swap_pair(node_t *head)
 {
     for (node_t **node = &head; *node && (*node)->next;
-        //BB1 = (e)
-        node = &(*node)->next->next
+        node = &(*node)->next->next //BB1
         ) {
         node_t *tmp = *node;
-        //BB2 = (c)
-        *node = (*node)->next;
+        *node = (*node)->next; //BB2
 
         tmp->next = (*node)->next;
         (*node)->next = tmp;
@@ -63,8 +59,7 @@ node_t *reverse(node_t *head)
     node_t *cursor = NULL;
     while (head) {
         node_t *next = head->next;
-        //CCC = (b)
-        head->next = cursor, cursor = head;
+        head->next = cursor, cursor = head; //CCC
         head = next;
     }
     return cursor;
